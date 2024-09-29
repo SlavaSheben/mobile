5de4c29f7367d75c9fa35cb5e9d9679732922585
SlavaIvanchuk <91247140+SlavaSheben@users.noreply.github.com>
2024-09-29 22:17:14 +0900

    Add files via upload

---
+++ 6 practic/src/12.kt
@@ -0,0 +1,7 @@
+import kotlin.random.Random
+fun main(){
+    val random = Array(10){ Random.nextInt(100)}
+    println(random.contentToString())
+    random.shuffle()
+    println(random.contentToString())
+}
